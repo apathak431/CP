6c2cd112fec2289c784fa20319ff62d0165e7490
apathak431 <35342085+apathak431@users.noreply.github.com>
2020-08-03 20:40:50 +0530

    Daily Coding Problem 3

---
+++ Daily Coding Problems/DCP3_SmallestPositiveInt.java
@@ -0,0 +1,47 @@
+/*
+This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space. 
+In other words, find the lowest positive integer that does not exist in the array. The array can contain 
+duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class DCP3_SmallestPositiveInt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	System.out.println("Enter the length of Array:- ");
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	System.out.println("Enter the content of Array:- ");
+	int[] arr=new int[n];
+	for(int i=0;i<n;i++){
+	    arr[i]=sc.nextInt();
+	}
+	int flag=0;
+	int j=0;
+	while(flag==0){
+	    for(int i=0;i<n;i++){
+	        if(arr[i]==j){
+	           flag=1;
+	        }
+	    }
+	    if(flag==1){
+	        flag=0;
+	        j++;
+	    }
+	    else break;
+	}
+	System.out.println("Smallest positive number missing is :"+j);
+	
+	}
+}
