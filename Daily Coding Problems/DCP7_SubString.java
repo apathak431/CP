c852d729b1a2b5369b0e8703c0d8b47450fe2c64
apathak431 <35342085+apathak431@users.noreply.github.com>
2020-08-07 12:02:58 +0530

    Create DCP7_SubString.java

---
+++ Daily Coding Problems/DCP7_SubString.java
@@ -0,0 +1,45 @@
+/*
+This problem was asked by Twitter.
+
+Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+
+For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+
+*/
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+class DCP7_SubString
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Checker String:");
+		String check=sc.next();
+		
+		System.out.println("Enter lenght of array:");
+		int n=sc.nextInt();
+		
+		String[] arr=new String[n];
+	
+		
+		System.out.println("Enter elements of array:");
+	
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.next();
+		    if(check.equals(arr[i].substring( 0,check.length()))==true ){
+		        
+		        System.out.println(arr[i]);
+		        
+		    }
+		    
+		}
+		
+	}
+}
+
